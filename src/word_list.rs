f7d673b6ed0891f42a797439660ff043e0f03845
Max Jacobson <max@hardscrabble.net>
2017-01-23 10:35:54 -0500

    Make sure the word doesn't have duplicate chars

--- src/word_list.rs
+++ src/word_list.rs
@@ -38,6 +38,29 @@ impl WordList {
     }
 
     fn valid(word: &str) -> bool {
-        word.chars().count() == 5 && char::is_lowercase(word.chars().nth(0).unwrap())
+        word.chars().count() == 5 &&
+            char::is_lowercase(word.chars().nth(0).unwrap()) &&
+            WordList::contains_no_duplicates(word)
+    }
+
+    fn contains_no_duplicates(word: &str) -> bool {
+        let mut chars: Vec<char> = word.chars().collect();
+        chars.sort();
+        let orig_count = chars.len();
+        chars.dedup();
+        orig_count == chars.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use WordList;
+
+    #[test]
+    fn test_valid() {
+        assert_eq!(WordList::valid("abhor"), true);
+        assert_eq!(WordList::valid("lemon"), true);
+        assert_eq!(WordList::valid("jolly"), false);
+        assert_eq!(WordList::valid("folly"), false);
     }
 }
