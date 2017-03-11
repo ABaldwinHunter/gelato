0ddc143f74a47acd5a7523c65abadf7638ba9b9c
Max Jacobson <max@hardscrabble.net>
2017-03-11 15:05:46 -0500

    Add basic file reading, exclude capitalized words.
    Commited with @abaldwinhunter

--- makedict/src/main.rs
+++ makedict/src/main.rs
@@ -1,4 +1,15 @@
 use std::env;
+use std::io::prelude::*;
+use std::fs::File;
+
+struct Expander {
+}
+
+impl Expander {
+    fn via(word: &str) -> Vec<&str> {
+        vec!["hello"]
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +19,24 @@ fn main() {
 
     println!("full_dict_path: {}", full_dict_path);
     println!("computed_dict_path: {}", computed_dict_path);
+
+    let mut f = File::open(full_dict_path).unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+
+    // println!("Input dictionary contents: {:?}", s);
+
+    let mut words: Vec<&str> = vec![];
+
+    for word in s.lines() {
+        // if !&word.capitalized?
+        if &word == &word.to_lowercase() {
+            words.push(&word);
+            for expanded_word in Expander::via(&word) {
+                words.push(expanded_word);
+            }
+        }
+    }
+
+    println!("{:?}", words);
 }
